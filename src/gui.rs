3e8ba34f51f397a31ef5ecb1b16110289892de35
tpaau-17DB <tpaau-17DB@tutamail.com>
2025-12-13 12:20:46 +0100

    Organize command execution in main

--- src/gui.rs
+++ src/gui.rs
@@ -4,18 +4,28 @@ use crate::argparse::GuiCommand;
 
 pub enum GuiExitStatus {
     Ok,
-    StoppedUnexpectedly,
 }
 
 impl Display for GuiExitStatus {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             GuiExitStatus::Ok => write!(f, "GUI exited succesfully"),
-            GuiExitStatus::StoppedUnexpectedly => write!(f, "GUI stopped unexpectedly"),
         }
     }
 }
 
-pub fn start(cmd: GuiCommand) -> Result<GuiExitStatus, GuiExitStatus> {
-    Err(GuiExitStatus::StoppedUnexpectedly)
+pub enum GuiError {
+    StoppedUnexpectedly,
+}
+
+impl Display for GuiError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            GuiError::StoppedUnexpectedly => write!(f, "GUI stopped unexpectedly"),
+        }
+    }
+}
+
+pub fn start() -> Result<GuiExitStatus, GuiError> {
+    Err(GuiError::StoppedUnexpectedly)
 }
--- src/main.rs
+++ src/main.rs
@@ -5,35 +5,48 @@ mod gui;
 use argparse::parse_args;
 use log::{error, info};
 
-use crate::argparse::{Command, DaemonCommand};
+use crate::argparse::{Command, DaemonCommand, GuiCommand};
 
 fn main() {
     let args = parse_args();
 
     if let Some(command) = args.command {
         match command {
-            Command::Daemon { command } => match command {
-                DaemonCommand::Start => match daemon::start() {
-                    Ok(status) => {
-                        info!("Daemon exited: {status}");
+            Command::Daemon { command } => {
+                if let DaemonCommand::Start = command {
+                    match daemon::start() {
+                        Ok(status) => {
+                            info!("Daemon exited: {status}");
+                        }
+                        Err(e) => {
+                            error!("Daemon failed: {e}");
+                        }
                     }
-                    Err(e) => {
-                        error!("Daemon failed: {e}");
+                } else {
+                    match mpipc::exec_daemon_command(command.into()) {
+                        Ok(response) => {}
+                        Err(e) => {
+                            error!("Failed executing daemon command: {e}")
+                        }
                     }
-                },
-                _ => {
-                    mpipc::exec_daemon_command(command.into());
                 }
-            },
-            Command::Gui { command } => match gui::start(command) {
-                Ok(status) => {
-                    info!("GUI exited: {status}");
+            }
+            Command::Gui { command } => {
+                if let GuiCommand::Start = command {
+                    match gui::start() {
+                        Ok(status) => {
+                            info!("GUI exited: {status}");
+                        }
+                        Err(e) => {
+                            error!("GUI failed: {e}");
+                        }
+                    }
+                } else {
+                    panic!("GUI command execution not supported!");
                 }
-                Err(e) => {
-                    error!("GUI failed: {e}");
-                }
-            },
+            }
         }
     } else {
+        panic!("Default command not yet supported!");
     }
 }
