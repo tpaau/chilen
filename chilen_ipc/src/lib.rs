0a958026f35ad0767ddeb5ce7c41b918d84172a6
tpaau <tpaau-17db@tutamail.com>
2026-07-03 15:39:33 +0200

    Implement `std::error::Error` for `chilen_ipc::Error`

--- chilen_ipc/src/lib.rs
+++ chilen_ipc/src/lib.rs
@@ -133,6 +133,8 @@ pub enum Error {
     PlaylistExportFailed,
 }
 
+impl std::error::Error for Error {}
+
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
