d942b25b61afe66c1ebd695dacf6d73c6f6753f4
tpaau <tpaau-17db@tutamail.com>
2026-08-11 22:39:34 +0200

    Remove the `advanced` feature from `iced_m3`

--- iced_m3/src/widget/mod.rs
+++ iced_m3/src/widget/mod.rs
@@ -1,6 +1,5 @@
 pub mod button;
 pub mod dialog;
-#[cfg(feature = "advanced")]
 pub mod drop_down_menu;
 pub mod fab_menu;
 pub mod navbar;
@@ -38,7 +37,6 @@ where
     Dialog::new(is_open, base, content, palette)
 }
 
-#[cfg(feature = "advanced")]
 #[must_use]
 pub fn drop_down_menu<'a, Message, Theme, Renderer>(
     trigger: impl Fn(bool) -> Element<'a, Message, Theme, Renderer> + 'a,
